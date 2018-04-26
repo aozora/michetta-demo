205b26a00bf98cfa9702e942a288c52b566fa6b7
marcellop <marcello.palmitessa@gmail.com>
2018-04-26 11:09:37 +0200

    wip components

---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <nav>
+      <a href="/"></a>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "SiteHeader"
+}
+</script>
+
+<style scoped>
+
+</style>
