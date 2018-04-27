5ed4d1508d13eaa8a993c5c871a407dcbb4efc08
marcellop <marcello.palmitessa@gmail.com>
2018-04-27 11:59:10 +0200

    completed moving to vue-meta

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,8 +1,14 @@
 module.exports = {
   title: 'Demo Michetta',
   description: 'Wow look at that bread!',
-  head: [
-    ['link', { rel: 'icon', href: `/favicon.ico` }]
-  ]
+  metaInfo: {
+    link: [
+      // { rel: 'stylesheet', href: '/css/index.css' },
+      { rel: 'favicon', href: '/favicon.ico' }
+    ]
+  }
+  // head: [
+  //   ['link', { rel: 'icon', href: `/favicon.ico` }]
+  // ]
 };
 
